0e764d806972094b99d98c36908df828069d5b24
BlindPenguin <f.holzner@limeonfire.de>
2020-06-11 10:27:32 +0200

    Some simple styling

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,1 +1,22 @@
-<h1>{{ $user->name }}</h1>
+@extends('layouts.app')
+@section('content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <strong>{{ $user->name }}</strong>
+                    {{ link_to_route('users.edit', 'Edit user', [$user->id], ['class' => 'btn btn-sm btn-warning float-right']) }}
+                    {{ link_to_route('users.index', 'Back to index', [], ['class' => 'btn btn-sm btn-primary float-right']) }}
+                </div>
+                <div class="card-body">
+                    <ul>
+                        <li>E-Mail: {{ $user->email }}</li>
+                        <li>
+                            Roles: @foreach($user->roles as $role) <div class="badge badge-primary">{{ $role->name }}</div> @endforeach
+                            </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
